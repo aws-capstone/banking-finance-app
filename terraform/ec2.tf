4017472eef816adf88100020699949b8106e7acc
Nikita <nikitaks997@gmail.com>
2024-12-16 23:37:22 +0100

    Changed key name

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,11 +3,11 @@ resource "tls_private_key" "mykey" {
 }
 
 resource "aws_key_pair" "aws_key" {
-  key_name   = "web-key"
+  key_name   = "web-key-ec2"
   public_key = tls_private_key.mykey.public_key_openssh
 
 provisioner "local-exec" {
-    command = "echo '${tls_private_key.mykey.private_key_openssh}' > ./web-key.pem"
+    command = "echo '${tls_private_key.mykey.private_key_openssh}' > ./web-key-ec2.pem"
     
 }
 }
@@ -15,7 +15,7 @@ provisioner "local-exec" {
 resource "aws_instance" "myec2" {
   ami           = "ami-005fc0f236362e99f"
   instance_type = "t2.micro"
-  key_name = "web-key"
+  key_name = "web-key-ec2"
   subnet_id = aws_subnet.subnet-1.id
   vpc_security_group_ids = [aws_security_group.sa-sg.id]
   tags = {
