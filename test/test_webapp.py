37f221439877e3ac5e0a14b411a8fef87013d485
Nikita Kashyap <161322388+nikitaks97@users.noreply.github.com>
2024-12-17 22:33:55 +0100

    Update test_webapp.py

--- test/test_webapp.py
+++ test/test_webapp.py
@@ -12,8 +12,11 @@ chrome_options.add_argument("--headless")  # Run headless Chrome
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
+chrome_binary_path = "/usr/local/bin/chrome-linux64/"
+chrome_options.binary_location = chrome_binary_path
+
 # Path to your ChromeDriver
-service = Service('/usr/local/bin/chromedriver')
+service = Service('/usr/local/bin/chromedriver_linux64/')
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # Retrieve the hostname from the environment variable 
